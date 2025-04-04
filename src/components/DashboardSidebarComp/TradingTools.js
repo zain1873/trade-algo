4da357129ff73f97664e443025454d68a70e01b1
kaglur <prodevelopers333@gmail.com>
2025-04-04 17:25:01 +0500

    changes

--- src/components/DashboardSidebarComp/TradingTools.js
+++ src/components/DashboardSidebarComp/TradingTools.js
@@ -4,7 +4,16 @@ import React from "react";
 function TradingTools() {
   return (
     <div>
-        Trading tools
+        <div className="heatmap-container">
+        <iframe
+          title="Finviz Heatmap"
+          src="https://finviz.com/map.ashx?t=sec"
+          className="heatmap-iframe"
+          width="100%"
+          height="600"
+          frameBorder="0"
+        ></iframe>
+      </div>
     </div>
   );
 }
