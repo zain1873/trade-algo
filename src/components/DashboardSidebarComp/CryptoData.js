781dd97f6a9237876af388619f8e44366b6f0d6c
kaglur <prodevelopers333@gmail.com>
2025-04-04 17:34:18 +0500

    changes

--- src/components/DashboardSidebarComp/CryptoData.js
+++ src/components/DashboardSidebarComp/CryptoData.js
@@ -4,12 +4,16 @@ function CryptoData() {
     return(
         <div className="data_visualization">
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        Crypto Data
-
-                    </div>
-                </div>
+            <div className="heatmap-container">
+        <iframe
+          title="Finviz Heatmap"
+          src="https://finviz.com/map.ashx?t=sec"
+          className="heatmap-iframe"
+          width="100%"
+          height="600"
+          frameBorder="0"
+        ></iframe>
+      </div>
             </div>
         </div>
     )
--- src/components/DashboardSidebarComp/ForexData.js
+++ src/components/DashboardSidebarComp/ForexData.js
@@ -4,12 +4,16 @@ function ForexData() {
   return (
     <div className="data_visualization">
     <div className="container">
-        <div className="row">
-            <div className="col-lg-12">
-                Forex Data
-
-            </div>
-        </div>
+    <div className="heatmap-container">
+        <iframe
+          title="Finviz Heatmap"
+          src="https://finviz.com/map.ashx?t=sec"
+          className="heatmap-iframe"
+          width="100%"
+          height="600"
+          frameBorder="0"
+        ></iframe>
+      </div>
     </div>
 </div>
   )
