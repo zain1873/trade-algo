727695ce523a69edff339d5152d19c7d9ad5cdc4
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-04-25 18:53:52 +0500

    ada

--- src/components/DashboardSidebarComp/LiveSessionChat.js
+++ src/components/DashboardSidebarComp/LiveSessionChat.js
@@ -41,7 +41,7 @@ const TradingPlatform = () => {
               />
               <div>
                 <div className="d-flex align-items-center">
-                  <span>Dane Glisek</span>
+                  <span>Lan M</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
@@ -53,7 +53,7 @@ const TradingPlatform = () => {
                     <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                   </svg>
                 </div>
-                <div className="text-secondary small">TradeAlgo Analyst</div>
+                <div className="text-secondary small">Analyst</div>
               </div>
             </div>
 
@@ -78,7 +78,7 @@ const TradingPlatform = () => {
                 <h6>Live Trading Sessions</h6>
                 <p className="small text-secondary">
                   These group training sessions are designed to explain how to
-                  take full advantage of the alerts Trade Algo has to offer.
+                  take full advantage of the alerts Valourwealth has to offer.
                 </p>
 
                 <div className="small">
