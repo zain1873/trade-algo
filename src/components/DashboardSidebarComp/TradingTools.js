30cb34adbc93c491aeaecfb0efb4f623180b2c81
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-04-27 14:05:48 +0500

    ada

--- src/components/DashboardSidebarComp/TradingTools.js
+++ src/components/DashboardSidebarComp/TradingTools.js
@@ -1,19 +1,38 @@
-import React from "react";
+// import React from "react";
 
+// function TradingTools() {
+//   return (
+//     <div>
+//         <div className="heatmap-container">
+//         <iframe
+//           title="Finviz Heatmap"
+//           src="https://finviz.com/map.ashx?t=sec"
+//           className="heatmap-iframe"
+//           width="100%"
+//           height="600"
+//           frameBorder="0"
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default TradingTools;
+
+import React from "react";
 
 function TradingTools() {
   return (
     <div>
-        <div className="heatmap-container">
-        <iframe
-          title="Finviz Heatmap"
-          src="https://finviz.com/map.ashx?t=sec"
+      <div className="heatmap-container">
+        <img
+          src="https://finviz.com/export.ashx?id=map_sec.png"
+          alt="Finviz Heatmap"
           className="heatmap-iframe"
           width="100%"
           height="600"
-          frameBorder="0"
-        ></iframe>
-      </div>
+        />
+      </div>
     </div>
   );
 }
