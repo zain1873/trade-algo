04a327e890c55d0feb6b4d15e9cd0b6044673712
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-05-13 10:40:56 +0500

    a

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -180,9 +180,9 @@ function Login() {
   };
 
   return (
-    <div className="login-container p-4">
+    <div className="login-container">
       <div className="container-fluid">
-        <div className="row">
+        <div className="row" style={{position: 'relative'}}>
           <div className="col-lg-6 p-0">
             <div className="login-img">
               <img
